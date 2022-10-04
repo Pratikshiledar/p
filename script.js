f872d41793cd905ed109bd7e585a230e787f2db5
Pranav Shiledar <pranavshiledar@gmail.com>
2022-10-05 00:08:58 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+function texteditor()
+              {
+                document.getElementById('title').innerHTML= "Welcome World"
+              }
+ function buttonclicked()
+ {
+    alert('Button is clicked');
+ }
